90eb6346ead67eec9d7170c0da43c2d1914347e2
Vishal Gauba <vishal.gauba@gmail.com>
2022-04-28 23:03:27 +0530

    fix: use event_time only, use amplitude_id instead of user_id

--- amplitude-export.py
+++ amplitude-export.py
@@ -33,62 +33,59 @@ def write_to_csv(single_date, user_durations):
 
   with open(output_file_name, 'a') as csv_file:
     writer_object = writer(csv_file)
-    for user_id in user_durations:
-      writer_object.writerow([single_date.strftime("%Y-%m-%d"), user_id, user_durations[user_id]])
+    for amplitude_id in user_durations:
+      writer_object.writerow([single_date.strftime("%Y-%m-%d"), amplitude_id, user_durations[amplitude_id]['user_id'], user_durations[amplitude_id]['total_duration']])
     csv_file.close()
 
 def process_user_durations(session_durations):
   user_durations = {}
   
   for session_id in session_durations:
+    amplitude_id = session_durations[session_id]['amplitude_id']
     user_id = session_durations[session_id]['user_id']
     start = session_durations[session_id]['start']
     end = session_durations[session_id]['end']
 
     duration = (end - start).total_seconds() / 60
 
-    if user_id not in user_durations:
+    if amplitude_id not in user_durations:
       total_duration = round(duration, 2)
     else:
-      total_duration = round(user_durations[user_id] + duration, 2)
+      total_duration = round(user_durations[amplitude_id]['total_duration'] + duration, 2)
 
     if total_duration <= 0:
       continue
 
-    user_durations[user_id] = total_duration
+    user_durations[amplitude_id] = {
+      'user_id': user_id,
+      'total_duration': total_duration
+    }
 
   return user_durations
 
-def process_event_record(line, session_durations, active_user_ids):
+def process_event_record(line, session_durations, active_users_amplitude_ids):
   event = json.loads(line)
 
-  if 'user_id' not in event:
-    return
-
+  amplitude_id = str(event['amplitude_id'])
   session_id = event['session_id']
-  user_id = event['user_id']
-  event_time = datetime.fromisoformat(event['client_event_time'])
 
-  # only process for active users
-  if user_id not in active_user_ids:
+  if (
+    # only process for active users
+    (amplitude_id not in active_users_amplitude_ids) or
+    # ignore server side "leave call" events
+    (session_id == -1)
+  ):
     return
 
-  # ignore server side "leave call" events
-  if session_id == -1:
-    return
-
-  # adjust event time (ref: https://amplitude.com/blog/dont-trust-client-data)
-  event_time += (datetime.fromisoformat(event['server_upload_time']) - datetime.fromisoformat(event['client_upload_time']))
-
-  # # outlier entries with huge gap between upload and event times
-  if(abs((datetime.fromisoformat(event['client_upload_time']) - event_time).total_seconds()) > 0.9*60*60):
-    event_time = datetime.fromisoformat(event['server_upload_time'])
+  user_id = event.get('user_id', '')
+  event_time = datetime.fromisoformat(event['event_time'] + "+00:00")
 
   # first entry of a session
   if session_id not in session_durations:
     session_durations[session_id] = {
       'start': event_time,
       'end': event_time,
+      'amplitude_id': amplitude_id,
       'user_id': user_id
     }
   else:
@@ -102,7 +99,7 @@ def process_event_record(line, session_durations, active_user_ids):
     elif event_time > end:
       session_durations[session_id]['end'] = event_time
 
-def process_session_durations(active_user_ids):
+def process_session_durations(active_users_amplitude_ids):
   session_durations = {}
 
   for (root, _, files) in os.walk(temp_data_folder, topdown=True):
@@ -110,7 +107,7 @@ def process_session_durations(active_user_ids):
       if file.endswith('.gz'):
         with gzip.open(os.path.join(root, file), 'r') as fin:
           for line in fin:
-            process_event_record(line, session_durations, active_user_ids)
+            process_event_record(line, session_durations, active_users_amplitude_ids)
 
   return session_durations
 
@@ -131,25 +128,25 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 def get_active_users():
-  active_user_ids = set()
+  active_users_amplitude_ids = set()
 
   with open(active_users_cohort_filename, 'r') as csv_file:
     csv_reader = reader(csv_file, delimiter=',')
     for row in csv_reader:
-      active_user_ids.add(row[1])
+      active_users_amplitude_ids.add(row[0])
 
-  return active_user_ids
+  return active_users_amplitude_ids
 
 # entrypoint
 
 def main():
-  active_user_ids = get_active_users()
+  active_users_amplitude_ids = get_active_users()
 
   for single_date in tqdm(daterange(start_date, end_date)):
     event_date = single_date.strftime("%Y%m%d")
 
     download_event_data(event_date)
-    session_durations = process_session_durations(active_user_ids)
+    session_durations = process_session_durations(active_users_amplitude_ids)
     user_durations = process_user_durations(session_durations)
     write_to_csv(single_date, user_durations)
 
